dc7138fcaf2a9f2f9928f52187f437cf04bb2661
alois-renggli <alois.renggli@gmail.com>
2018-09-17 16:55:19 -0500

    udpated consoleexercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
 //        The value of pi is approximately 3.14.
-        System.out.format("The value of pi is approximately %s", pi);
+        System.out.format("The value of pi is approximately %s \n", pi);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter an integer: ");
+        int userInput = scanner.nextInt();
+
+        System.out.println("You entered: --> " + userInput + " <--");
     }
 }
