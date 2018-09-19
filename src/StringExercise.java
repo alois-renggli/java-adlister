07255eaf50d61c0bcd723feca35b32c29c3c703d
alois-renggli <alois.renggli@gmail.com>
2018-09-19 14:40:05 -0500

    updated String exercise and Bob exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        String education = "We don't need no education";
+        String control = "We don't need no thought control";
+        System.out.println(education);
+        System.out.println(control);
+
+        String checkOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkOut);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backslashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+    }
+}
