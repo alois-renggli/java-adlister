bbcc18457fee76ebaeb48df92d9e7054fe0d23d5
alois-renggli <alois.renggli@gmail.com>
2018-09-21 16:57:23 -0500

    completed all Java 2 intro exerceises

---
+++ src/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+    private String name;
+//     returns the person's name
+    public String getName(){
+        return name;
+    };
+    // changes the name property to the passed value
+    public void setName(String name){
+        this.name = name;
+    };
+
+    // prints a message to the console using the person's name
+    public void sayHello(){
+        System.out.println("Hello, " + this.name);
+    };
+
+//    The class should have a constructor that accepts a string
+// value and sets the person's name to the passed string.
+//
+//    Create a main method on the class that creates a new Person
+// object and tests the above methods.
+    public Person(String name){
+        setName(name);
+        sayHello();
+    }
+
+    public static void main(String[] args) {
+//        Person louie = new Person("Louie");
+//        System.out.println(louie.name);
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,94 @@
+package util;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Input {
+
+//    Create an input validation class
+//
+//    Create directory inside of src named util.
+// Inside of util, create a class named Input that has a private
+// property named scanner. When an instance of this object is created,
+// the scanner property should be set to a new instance of the Scanner class.
+
+// The class should have the following methods, all of which return command
+// line input from the user:
+//
+//    String getString()
+//    boolean yesNo()
+//    int getInt(int min, int max)
+//    int getInt()
+//    double getDouble(double min, double max)
+//    double getDouble()
+//    The yesNo method should return true if the user
+// enters y, yes, or variants thereof, and false otherwise.
+//
+// The getInt(int min, int max) method should
+// keep prompting the user for input until they give an integer
+// within the min and max. The getDouble method should do the same thing,
+// but with decimal numbers.
+//
+//    Create another class named InputTest that has a static main method
+// that uses all of the methods from the Input class.
+
+    private Scanner scanner = new Scanner(System.in);
+
+//    public Input {
+//        scanner = new Scanner(System.in).useDelimiter("\n");
+//    }
+
+//    new Scanner(System.in);
+
+    String getString() {
+        System.out.println("Enter a string:");
+        return scanner.nextLine();
+    }
+    boolean yesNo(){
+        System.out.println("Confirm Yes or No:");
+        String input = scanner.nextLine();
+        if(input.toLowerCase().contains("y")){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    int getInt(int min, int max){
+        System.out.printf("Enter a number from %s to %s:\n", min, max);
+        int input = scanner.nextInt();
+        if (input > max || input < min){
+            System.out.println("Invalid entry! Input another number");
+            return getInt(min, max);
+        }
+        else{
+            System.out.printf("You entered %s!\n", input);
+            return input;
+        }
+    }
+    public int getInt(){
+        System.out.printf("Enter a number:\n");
+        int input = scanner.nextInt();
+        System.out.printf("You entered %s!\n", input);
+        return input;
+    }
+    double getDouble(double min, double max){
+        System.out.printf("Enter a number from %s to %s:\n", min, max);
+        double input = scanner.nextInt();
+        if (input > max || input < min){
+            System.out.println("Invalid entry! Input another number\n");
+            return getDouble(min, max);
+        }
+        else{
+            System.out.printf("You entered %s!", input);
+            return input;
+        }
+    }
+    double getDouble(){
+        System.out.printf("Enter a number:\n");
+        double input = scanner.nextInt();
+        System.out.printf("You entered %s!", input);
+        return input;
+    }
+}
