87b15ef57a25f872f8c44219c5025a733c50b273
alois-renggli <alois.renggli@gmail.com>
2018-09-28 10:31:38 -0500

    Completed Exceptions exercise

---
+++ src/SandBox/SleepIn.java
@@ -0,0 +1,22 @@
+
+public class SleepIn{
+
+	public Boolean weekday;
+	public Boolean vacation;
+	public SleepIn (){
+	}
+
+	public Boolean sleepIn(Boolean weekday, Boolean vacation){
+			if (weekday != true || vacation == true){
+				return true;
+			}else{
+				return false;
+			}
+	}
+
+	public static void main (String[] args){
+		SleepIn test = new SleepIn();
+			System.out.println(test.sleepIn(true, false));
+	}
+
+}
