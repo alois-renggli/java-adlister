7f17731b710ba3a4cb8363021e63d4c92d624c29
alois-renggli <alois.renggli@gmail.com>
2018-10-10 14:18:20 -0500

    updated Hello World exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,32 +1,23 @@
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.*;
-//
-//
-//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-//
-//public class HelloWorldServlet extends HttpServlet {
-//    protected void doGet(HttpServletRequest req, HttpServletResponse res)  throws ServletException, IOException{
-//        res.setContentType("text/html");
-//        PrintWriter out = res.getWriter();
-//        out.println("<h1>Hello, World!</h1>");
-//    }
-//}
-
-import java.io.*;
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+    private int counter = 0;
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        counter+=1;
+        out.println("<h1>Hello, World!</h1><h3>Number of hits for this page: "+counter+"</h3>");
     }
 
 }
+
